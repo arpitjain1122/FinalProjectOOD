7bc8c692710122971820c1b1941f1796209ee14d
Arpit Jain <jain.arpit@northeastern.edu>
2023-04-10 06:40:19 -0400

    creating the structure of the project

---
+++ src/application/Models/Model.java
@@ -0,0 +1,5 @@
+package application.Models;
+
+public class Model {
+
+}
---
+++ src/module-info.java
@@ -0,0 +1,17 @@
+module BankingSystem {
+	requires javafx.controls;
+	requires javafx.fxml;
+	requires de.jensd.fx.glyphs.fontawesome;
+
+	// add require java.sql
+	// add org.xerial.sqlitejdbc; if required
+	opens application to javafx.graphics, javafx.fxml;
+
+	exports application;
+	exports application.Controller;
+	exports application.Controller.Admin;
+	exports application.Controller.Client;
+	exports application.Models;
+	exports application.Views;
+
+}
