127f1b9a86c2b5f72ce802278deb78082a683e21
Arpit Jain <jain.arpit@northeastern.edu>
2023-04-10 06:58:05 -0400

    t

--- src/application/Main.java
+++ src/application/Main.java
@@ -14,7 +14,6 @@ public class Main extends Application {
 			scene.getStylesheets().add(getClass().getResource("application.css").toExternalForm());
 			primaryStage.setScene(scene);
 			primaryStage.show();
-			System.out.println();
 		} catch (Exception e) {
 			e.printStackTrace();
 		}
