62c1ae2c9fa69dfe730518386157d71c3069fa0d
Arpit Jain <jain.arpit@northeastern.edu>
2023-04-11 18:02:41 -0400

    creating client dashboard left pane design complete

--- src/application/Controller/Client/ClientMenuController.java
+++ src/application/Controller/Client/ClientMenuController.java
@@ -1,5 +1,24 @@
 package application.Controller.Client;
 
-public class ClientMenuController {
+import java.net.URL;
+import java.util.ResourceBundle;
+
+import javafx.fxml.Initializable;
+import javafx.scene.control.Button;
+
+public class ClientMenuController implements Initializable {
+
+	public Button dashboard_btn;
+	public Button transaction_btn;
+	public Button account_btn;
+	public Button profile_btn;
+	public Button logout_btn;
+	public Button report_btn;
+
+	@Override
+	public void initialize(URL arg0, ResourceBundle arg1) {
+		// TODO Auto-generated method stub
+
+	}
 
 }
