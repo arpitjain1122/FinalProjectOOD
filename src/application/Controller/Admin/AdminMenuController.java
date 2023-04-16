85fe0d8b05aff8bae522c16bad0fb3df9d81b5db
Arpit Jain <jain.arpit@northeastern.edu>
2023-04-16 19:22:33 -0400

    Creating the user window in sceneB

---
+++ src/application/Controller/Admin/AdminMenuController.java
@@ -0,0 +1,21 @@
+package application.Controller.Admin;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+
+import javafx.fxml.Initializable;
+import javafx.scene.control.Button;
+
+public class AdminMenuController implements Initializable {
+	public Button create_client_btn;
+	public Button clients_btn;
+	public Button deposit_btn;
+	public Button logout_btn;
+
+	@Override
+	public void initialize(URL arg0, ResourceBundle arg1) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
