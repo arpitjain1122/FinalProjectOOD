fa7a9014278b41cba0f2b6bcf8146507afad43f3
Swarag Gutte <gutte.s@northeastern.edu>
2023-04-18 10:23:01 -0400

    Updated logi controller

--- src/application/Controller/LoginController.java
+++ src/application/Controller/LoginController.java
@@ -1,4 +1,4 @@
-package application.Controller;
+package application.Controllers;
 
 import java.net.URL;
 import java.util.ResourceBundle;
@@ -25,47 +25,46 @@ public class LoginController implements Initializable {
 	public void initialize(URL url, ResourceBundle resourceBundle) {
 		acc_selector.setItems(FXCollections.observableArrayList(AccountType.CLIENT, AccountType.ADMIN));
 		acc_selector.setValue(Model.getInstance().getViewFactory().getLoginAccountType());
-		acc_selector.valueProperty().addListener(
-				observable -> Model.getInstance().getViewFactory().setLoginAccountType(acc_selector.getValue()));
+		acc_selector.valueProperty().addListener(observable -> setAcc_selector());
 		login_btn.setOnAction(event -> onLogin());
 	}
 
-	   private void onLogin() {
-	        Stage stage = (Stage) error_lbl.getScene().getWindow();
-	        if (Model.getInstance().getViewFactory().getLoginAccountType() == AccountType.CLIENT){
-	            // Evaluate Client Login Credentials
-	            Model.getInstance().evaluateClientCred(payee_address_fld.getText(), password_fld.getText());
-	            if (Model.getInstance().getClientLoginSuccessFlag()){
-	                Model.getInstance().getViewFactory().showClientWindow();
-	                // Close the login stage
-	                Model.getInstance().getViewFactory().closeStage(stage);
-	            } else {
-	                payee_address_fld.setText("");
-	                password_fld.setText("");
-	                error_lbl.setText("No Such Login Credentials.");
-	            }
-	        } else {
-	            // Evaluate Admin Login Credentials
-	            Model.getInstance().evaluateAdminCred(payee_address_fld.getText(), password_fld.getText());
-	            if (Model.getInstance().getAdminLoginSuccessFlag()){
-	                Model.getInstance().getViewFactory().showAdminWindow();
-	                // Close the login stage
-	                Model.getInstance().getViewFactory().closeStage(stage);
-	            } else {
-	                payee_address_fld.setText("");
-	                password_fld.setText("");
-	                error_lbl.setText("No Such Login Credentials");
-	            }
-	        }
-	    }
+	private void onLogin() {
+		Stage stage = (Stage) error_lbl.getScene().getWindow();
+		if (Model.getInstance().getViewFactory().getLoginAccountType() == AccountType.CLIENT) {
+			// Evaluate Client Login Credentials
+			Model.getInstance().evaluateClientCred(payee_address_fld.getText(), password_fld.getText());
+			if (Model.getInstance().getClientLoginSuccessFlag()) {
+				Model.getInstance().getViewFactory().showClientWindow();
+				// Close the login stage
+				Model.getInstance().getViewFactory().closeStage(stage);
+			} else {
+				payee_address_fld.setText("");
+				password_fld.setText("");
+				error_lbl.setText("No Such Login Credentials.");
+			}
+		} else {
+			// Evaluate Admin Login Credentials
+			Model.getInstance().evaluateAdminCred(payee_address_fld.getText(), password_fld.getText());
+			if (Model.getInstance().getAdminLoginSuccessFlag()) {
+				Model.getInstance().getViewFactory().showAdminWindow();
+				// Close the login stage
+				Model.getInstance().getViewFactory().closeStage(stage);
+			} else {
+				payee_address_fld.setText("");
+				password_fld.setText("");
+				error_lbl.setText("No Such Login Credentials");
+			}
+		}
+	}
 
-	    private void setAcc_selector() {
-	        Model.getInstance().getViewFactory().setLoginAccountType(acc_selector.getValue());
-	        // Change Payee Address label accordingly
-	        if (acc_selector.getValue() == AccountType.ADMIN){
-	            payee_address_lbl.setText("Username:");
-	        } else {
-	            payee_address_lbl.setText("Payee Address:");
-	        }
-	    }
+	private void setAcc_selector() {
+		Model.getInstance().getViewFactory().setLoginAccountType(acc_selector.getValue());
+		// Change Payee Address label accordingly
+		if (acc_selector.getValue() == AccountType.ADMIN) {
+			payee_address_lbl.setText("Username:");
+		} else {
+			payee_address_lbl.setText("Payee Address:");
+		}
+	}
 }
