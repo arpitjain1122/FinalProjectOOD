aa483a494839f43b84901e7b83badc69196d003b
Swarag Gutte <gutte.s@northeastern.edu>
2023-04-19 07:14:44 -0400

    Modified folder structure of application

--- src/module-info.java
+++ src/module-info.java
@@ -1,18 +1,19 @@
 module BankingSystem {
 	requires transitive javafx.controls;
 	requires transitive javafx.fxml;
-	requires de.jensd.fx.glyphs.fontawesome;
+	requires transitive de.jensd.fx.glyphs.fontawesome;
 	requires transitive javafx.graphics;
 	requires transitive javafx.base;
+	requires transitive java.sql;
 
 	// add require java.sql
 	// add org.xerial.sqlitejdbc; if required
 	opens application to javafx.graphics, javafx.fxml;
 
 	exports application;
-	exports application.Controller;
-	exports application.Controller.Admin;
-	exports application.Controller.Client;
+	exports application.Controllers;
+	exports application.Controllers.Admin;
+	exports application.Controllers.Client;
 	exports application.Models;
 	exports application.Views;
 
